f12ad0a494e9447ca8c067cf6c20f294597f5be9
aureliewouy <aureliecedia@yahoo.com>
2020-11-18 15:26:27 +0000

    add header

---
+++ holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <sys/stat.h>
+
+#endif
