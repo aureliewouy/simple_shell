20ed047d859e792ec11682aad9a2b4d0da155578
Natalia Ponsard <2503@holbertonschool.com>
2020-11-19 15:52:37 +0000

    string_functions.c

--- string_functions.c
+++ string_functions.c
@@ -1,25 +1,31 @@
 #include "holberton.h"
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- * @str: character
+ * *_strdup - a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter.
+ * @str: the string.
  *
- * Return: a pointer to a new string which is a duplicate of the string
+ * Return: a pointer to a new string or NULL.
  */
+
 char *_strdup(char *str)
 {
-	int i, j = 0;
-	char *ptr;
+	char *new_s;
+	int size = 0;
+	int i = 0;
 
 	if (str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-		j++;
-	ptr = malloc(sizeof(char) * j + 1);
-	if (ptr == NULL)
+	while (str[size])
+		size++;
+	new_s = malloc((size + 1) * sizeof(char));
+	if (new_s == NULL)
 		return (NULL);
-	for (i = 0; i < j; i++)
-		ptr[i] = str[i];
-	ptr[j] = '\0';
-	return (ptr);
+	while (i < size)
+	{
+		new_s[i] = str[i];
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
 }
