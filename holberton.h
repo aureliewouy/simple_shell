593f757be03b8304cf59564966bb50825b82ff0e
aureliewouy <aureliecedia@yahoo.com>
2020-11-21 13:31:21 +0000

    add new prototypes

--- holberton.h
+++ holberton.h
@@ -15,13 +15,16 @@ int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 int _strcmp(char *s1, char *s2);
 
+/*Free and malloc */
+void free_grid(char **grid);
+
 /*basic shell*/
 
 void create_child_pid(char **av, char **argv);
-int print_env(char **environ);
+char *_getenv(const char *name);
+char **array_directory_path(char *path_folder[]);
+char * verify_path(char **av);
 void print_string(char *env);
-
-
-
+int print_env(char **environ);
 
 #endif
