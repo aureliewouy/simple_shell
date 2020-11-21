f293a79b28f7f0a6c3d0a2e2d9f7e4127f68177b
Natalia Ponsard <2503@holbertonschool.com>
2020-11-21 13:38:03 +0000

    Function gets environement

---
+++ _getenv.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _getenv - gets an environment variable
+ * get value after "=""
+ * @name: name of the variable user enter
+ *
+ * Return: pointer to a string (value of the variable)
+ */
+char *_getenv(const char *name)
+{
+	char *environ_string;
+	char *value = NULL;
+	int i = 0;
+
+	while (environ[i] != '\0')
+	{
+		/*from array to the chain of characters*/
+		environ_string = _strdup(environ[i]);
+		/*selects the part of the line before =*/
+		value = strtok(environ_string, "=");
+		/*compare two strings*/
+		if (_strcmp(name, value) == 0)
+		{
+			return (strtok(NULL, "="));
+		}
+		i++;
+		/*environ[i] = environ_string;*/
+		free(environ_string);
+	}
+	return (NULL);
+}
